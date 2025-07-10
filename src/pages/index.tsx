925772d39a416c53c5f64ab3bfcdf6c1586c2837
ooliveiratg <menezesth16@gmail.com>
2025-07-09 21:32:30 -0300

    feat(home): added image carousel and styling to the home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,56 @@
+import { HomeContainer, Product } from '../styles/pages/home';
+import Image from 'next/image';
+import camisa1 from '../assets/camisetas/Shirt-1.png';
+import camisa2 from '../assets/camisetas/Shirt-2.png';
+import camisa3 from '../assets/camisetas/Shirt.png';
+import {useKeenSlider} from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css';
+
+export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides:{
+      perView: 3,
+      spacing: 48,
+      
+    }
+  })
+  return (
+    <HomeContainer ref={sliderRef} className='keen-slider'>
+      <Product  className='keen-slider__slide'>
+        <Image src={camisa1} width={520} height={480} alt={''}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+       <Product className='keen-slider__slide'>
+        <Image src={camisa2} width={520} height={480} alt={''}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+       <Product className='keen-slider__slide'>
+        <Image src={camisa3} width={520} height={480} alt={''}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camisa3} width={520} height={480} alt={''}/>
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
+}
